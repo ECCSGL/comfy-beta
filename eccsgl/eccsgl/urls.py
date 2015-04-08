6e18d30996080c93f88ea8b7adc1d241c3dfb450
Chalrus <chalrus.eccsgl@gmail.com>
2015-04-08 12:18:32 -0400

    Statics on server

--- eccsgl/eccsgl/urls.py
+++ eccsgl/eccsgl/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import comfy.views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +15,4 @@ urlpatterns = patterns('',
     url(r'^account/(?P<hash>[0-9a-zA-Z]+)',comfy.views.account_incl_hash),
     url(r'^account/',comfy.views.account_excl_hash),
     url(r'^placebet/',comfy.views.place_bet),
-    url(r'^switchbet/',comfy.views.switch_bet)
-)
+    url(r'^switchbet/',comfy.views.switch_bet)) + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
